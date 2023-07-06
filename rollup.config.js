4d6db1a797ebfe3f2acfefab3ece3256a7a4e7ed
Evgeny Kovich <evgenykovich@Evgenys-MacBook-Air.local>
2023-07-06 17:15:45 +0300

    [HS-196][feat] - edited the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,56 @@
-import copy from 'rollup-plugin-copy'
-import rollupBaseConfig from '../../rollup.config.base'
-import packageJson from './package.json'
+import resolve from "@rollup/plugin-node-resolve";
+import builtins from "rollup-plugin-node-builtins";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import packageJson from "./package.json";
+import copy from "rollup-plugin-copy";
 
-export default rollupBaseConfig({
-  outputs: [
+const config = () => {
+  let plugins = [
+    builtins(),
+    commonjs({ sourceMap: false }),
+    resolve({ preferBuiltins: true }),
+    typescript({
+      clean: true,
+      useTsconfigDeclarationDir: true,
+      noEmitOnError: false,
+    }),
+    peerDepsExternal(),
+    terser({ format: { comments: false } }),
+    copy({
+      targets: [{ src: "src/types/**/*", dest: "lib/dts/types" }],
+    }),
+  ];
+
+  return [
     {
-      file: packageJson.main,
-      format: 'umd',
-      name: 'Pay'
+      input: "src/index.ts",
+      output: [
+        {
+          file: packageJson.main,
+          format: "umd",
+          name: "Pay",
+        },
+        {
+          file: packageJson.module,
+          format: "esm",
+        },
+      ],
+      plugins,
+      watch: {
+        include: "./src/**",
+        clearScreen: true,
+      },
     },
     {
-      file: packageJson.module,
-      format: 'esm'
-    }
-  ],
-  watch: {
-    include: './src/**',
-    clearScreen: false
-  },
-  additionalPlugins: [
-    copy({
-      targets: [{ src: 'src/types/**/*', dest: 'lib/dts/types' }]
-    })
-  ]
-})
+      input: "lib/dts/index.d.ts",
+      output: [{ file: "lib/index.d.ts", format: "es" }],
+      plugins: [dts()],
+    },
+  ];
+};
+
+export default config;
